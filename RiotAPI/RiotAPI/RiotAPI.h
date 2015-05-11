54942bb2382ab31e1ca020e67bf342c88d84d96b
James Glenn <jglenn1@gmail.com>
2015-05-11 15:35:38 -0500

    Hi

--- RiotAPI/RiotAPI/RiotAPI.h
+++ RiotAPI/RiotAPI/RiotAPI.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char RiotAPIVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <RiotAPI/PublicHeader.h>
 
 
+
