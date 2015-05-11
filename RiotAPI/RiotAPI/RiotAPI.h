f0f3eb6e3e9d3263dbdd29248e78cfde299d87b7
James Glenn <jglenn1@gmail.com>
2015-05-11 15:40:14 -0500

    Test

---
+++ RiotAPI/RiotAPI/RiotAPI.h
@@ -0,0 +1,19 @@
+//
+//  RiotAPI.h
+//  RiotAPI
+//
+//  Created by James Glenn on 5/11/15.
+//  Copyright (c) 2015 James Glenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RiotAPI.
+FOUNDATION_EXPORT double RiotAPIVersionNumber;
+
+//! Project version string for RiotAPI.
+FOUNDATION_EXPORT const unsigned char RiotAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RiotAPI/PublicHeader.h>
+
+
