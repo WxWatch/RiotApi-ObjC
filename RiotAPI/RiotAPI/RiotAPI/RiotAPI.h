0a99fbc014ada0cb1fed7c88b7953272ec4fe353
James Glenn <jglenn1@gmail.com>
2015-05-11 14:58:29 -0500

    Lets try again

--- RiotAPI/RiotAPI/RiotAPI/RiotAPI.h
+++ RiotAPI/RiotAPI/RiotAPI/RiotAPI.h
@@ -1,15 +1,13 @@
 //
-//  NSArray+JGUtil.h
+//  RiotAPI.h
 //  RiotAPI
 //
-//  Created by James Glenn on 5/1/15.
+//  Created by James Glenn on 5/11/15.
 //  Copyright (c) 2015 James Glenn. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface NSArray (JGUtil)
-
-- (NSData*)convertToData;
+@interface RiotAPI : NSObject
 
 @end
--- RiotAPI/RiotAPI/RiotAPI/RiotAPI.m
+++ RiotAPI/RiotAPI/RiotAPI/RiotAPI.m
@@ -1,13 +1,13 @@
 //
-//  BaseObject.m
+//  RiotAPI.m
 //  RiotAPI
 //
 //  Created by James Glenn on 5/11/15.
 //  Copyright (c) 2015 James Glenn. All rights reserved.
 //
 
-#import "BaseObject.h"
+#import "RiotAPI.h"
 
-@implementation BaseObject
+@implementation RiotAPI
 
 @end
