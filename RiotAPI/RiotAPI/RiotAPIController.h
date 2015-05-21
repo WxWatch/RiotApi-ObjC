241be6094a36493bc4e076c66a5332478fabdf2e
James Glenn <jglenn1@gmail.com>
2015-05-20 21:24:31 -0500

    All class methods :o

--- RiotAPI/RiotAPI/RiotAPIController.h
+++ RiotAPI/RiotAPI/RiotAPIController.h
@@ -19,6 +19,13 @@
 #import "PlayerStatsSummaryList.h"
 #import "ShardStatus.h"
 #import "StaticChampionList.h"
+#import "StaticItemList.h"
+#import "StaticLanguageStrings.h"
+#import "StaticMapData.h"
+#import "StaticMasteryList.h"
+#import "StaticRealm.h"
+#import "StaticRuneList.h"
+#import "StaticSummonerSpellList.h"
 
 @interface RiotAPIController : NSObject
 
@@ -46,7 +53,42 @@
 + (void)getMasterLeagueWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(League *))success failure:(void (^)(NSError *))failure;
 
 ///Static Data
++ (void)getStaticChampionsWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticChampionList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticChampionsWithLocale:(NSString*)locale version:(NSString*)version dataById:(BOOL)dataById champData:(NSString*)champData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticChampionList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticChampionByID:(NSInteger)championID withAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticChampion *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticChampionByID:(NSInteger)championID withLocale:(NSString*)locale version:(NSString*)version dataById:(BOOL)dataById champData:(NSString*)champData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticChampion *))success failure:(void (^)(NSError *))failure;
 
++ (void)getStaticItemsWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticItemList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticItemsWithLocale:(NSString*)locale version:(NSString*)version itemListData:(NSString*)itemListData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticItemList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticItemByID:(NSInteger)itemID withAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticItem *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticItemByID:(NSInteger)itemID withLocale:(NSString*)locale version:(NSString*)version itemListData:(NSString*)itemListData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticItem *))success failure:(void (^)(NSError *))failure;
+
++ (void)getLanguageStringsWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticLanguageStrings *))success failure:(void (^)(NSError *))failure;
++ (void)getLanguageStringsWithLocale:(NSString*)locale version:(NSString*)version withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticLanguageStrings *))success failure:(void (^)(NSError *))failure;
+
++ (void)getLanguagesWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(NSArray *))success failure:(void (^)(NSError *))failure;
+
++ (void)getMapWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticMapData *))success failure:(void (^)(NSError *))failure;
++ (void)getMapWithLocale:(NSString*)locale version:(NSString*)version withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticMapData *))success failure:(void (^)(NSError *))failure;
+
++ (void)getStaticMasteriesWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticMasteryList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticMasteriesWithLocale:(NSString*)locale version:(NSString*)version masteryListData:(NSString*)masteryListData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticMasteryList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticMasteryByID:(NSInteger)masteryID withAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticMastery *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticMasteryByID:(NSInteger)masteryID withLocale:(NSString*)locale version:(NSString*)version masteryListData:(NSString*)masteryListData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticMastery *))success failure:(void (^)(NSError *))failure;
+
++ (void)getStaticRealmWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticRealm *))success failure:(void (^)(NSError *))failure;
+
++ (void)getStaticRunesWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticRuneList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticRunesWithLocale:(NSString*)locale version:(NSString*)version runeListData:(NSString*)runeListData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticRuneList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticRuneByID:(NSInteger)runeID withAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticRune *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticRuneByID:(NSInteger)runeID withLocale:(NSString*)locale version:(NSString*)version runeListData:(NSString*)runeListData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticRune *))success failure:(void (^)(NSError *))failure;
+
++ (void)getStaticSummonerSpellsWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticSummonerSpellList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticSummonerSpellsWithLocale:(NSString*)locale version:(NSString*)version dataById:(BOOL)dataById spellData:(NSString*)spellData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticSummonerSpellList *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticSummonerSpellByID:(NSInteger)summonerSpellID withAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(StaticSummonerSpell *))success failure:(void (^)(NSError *))failure;
++ (void)getStaticSummonerSpellByID:(NSInteger)summonerSpellID withLocale:(NSString*)locale version:(NSString*)version dataById:(BOOL)dataById spellData:(NSString*)spellData withAPIKey:(NSString *)key regionEndpoint:(Endpoint *)endpoint success:(void (^)(StaticSummonerSpell *))success failure:(void (^)(NSError *))failure;
+
++ (void)getVersionWithAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(NSArray *))success failure:(void (^)(NSError *))failure;
 
 ///Status
 + (void)getShardsWithSuccess:(void (^)(NSArray *))success failure:(void (^)(NSError *))failure;
@@ -76,6 +118,7 @@
 ///Team
 + (void)getTeamsForSummonerIDs:(NSArray*)summonerIDs withAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(NSDictionary *))success failure:(void (^)(NSError *))failure;
 + (void)getTeamsForTeamIDs:(NSArray*)teamIDs withAPIKey:(NSString*)key regionEndpoint:(Endpoint*)endpoint success:(void (^)(NSDictionary *))success failure:(void (^)(NSError *))failure;
+
 ///Instance Methods
 
 @end
